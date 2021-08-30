d68c3fc0ec115b6392daa54275e9d58099e95ce1
Rachel Swain <Rachel.Swain@epa.vic.gov.au>
2021-08-30 17:08:51 +1000

    Formatting options for grid and checkerboard

--- data-raw/testing.R
+++ data-raw/testing.R
@@ -2,6 +2,7 @@
 library(ggplot2)
 library(dplyr)
 library(gt)
+library(pReen)
 
 
 #### Load data ####
@@ -32,14 +33,91 @@ for(i in 1:length(grid_list)){
 
 }
 
+# even rows and cols
+even_cols <- seq(from = 2, to = ncol(grid_df), by = 2)
+even_rows <- seq(from = 2, to = nrow(grid_df), by = 2)
+
+# odd rows and cols
+odd_cols <- seq(from = 1, to = ncol(grid_df), by = 2)
+odd_rows <- seq(from = 1, to = nrow(grid_df), by = 2)
+
+
+#### gt output
+
+## checkerboard
 grid_df %>%
   gt() %>%
   tab_options(
     column_labels.hidden = TRUE,
-    data_row.padding = 10
+    data_row.padding = 20,
+    table.font.size = px(13)
   ) %>%
   cols_width(
     everything() ~ px(100)
+  ) %>%
+  opt_table_font(
+    font = list(
+      google_font(name = "Work Sans")
+    )
+  ) %>%
+  tab_style(
+    style = cell_fill(color = "gray90"),
+    locations = cells_body(
+      columns = even_cols,
+      rows = odd_rows
+    )
+  ) %>%
+  tab_style(
+    style = cell_fill(color = "gray90"),
+    locations = cells_body(
+      rows = even_rows,
+      columns = odd_cols
+    )
+  ) %>%
+  tab_style(
+    style = cell_borders(
+      sides = "all",
+      color = "white",
+    ),
+    locations = cells_body(
+      columns = everything(),
+      rows = everything()
+    )
   )
 
+
+# grid
+grid_df %>%
+  gt() %>%
+  tab_options(
+    column_labels.hidden = TRUE,
+    data_row.padding = 20,
+    table.font.size = px(13)
+  ) %>%
+  cols_width(
+    everything() ~ px(100)
+  ) %>%
+  opt_table_font(
+    font = list(
+      google_font(name = "Work Sans")
+    )
+  ) %>%
+  tab_style(
+    style = cell_fill(color = "gray90"),
+    locations = cells_body(
+      rows = everything(),
+      columns = everything()
+    )
+  ) %>%
+  tab_style(
+    style = cell_borders(
+      sides = "all",
+      color = "white",
+      weight = px(4)
+    ),
+    locations = cells_body(
+      columns = everything(),
+      rows = everything()
+    )
+  )
 
